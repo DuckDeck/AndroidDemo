11d3358ee3f3ac9c468a277284301fc7b2363d3d
stan <3421902@qq.com>
2017-08-02 19:51:10 +0800

    add belle

---
+++ app/src/main/java/stan/androiddemo/project/belle/BelleActivity.kt
@@ -0,0 +1,13 @@
+package stan.androiddemo.project.belle
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import stan.androiddemo.R
+
+class BelleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_belle)
+    }
+}
