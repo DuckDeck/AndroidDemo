40c43d5017b44342290e8ecf772af7b25184abc4
Stan Hu <3421902@qq.com>
2018-05-18 21:37:51 +0800

    process code

---
+++ app/src/main/java/stan/androiddemo/Media/RecordVideo/VideoListActivity.kt
@@ -0,0 +1,13 @@
+package stan.androiddemo.Media.RecordVideo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import stan.androiddemo.R
+
+class VideoListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_list)
+    }
+}
