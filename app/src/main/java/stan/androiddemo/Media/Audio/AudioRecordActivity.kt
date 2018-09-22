6702034865b7cfa195bba401a2385541676c03c0
Stan Hu <3421902@qq.com>
2018-09-22 19:27:23 +0800

    add audio record

---
+++ app/src/main/java/stan/androiddemo/Media/Audio/AudioRecordActivity.kt
@@ -0,0 +1,13 @@
+package stan.androiddemo.Media.Audio
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import stan.androiddemo.R
+
+class AudioRecordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_audio_record)
+    }
+}
