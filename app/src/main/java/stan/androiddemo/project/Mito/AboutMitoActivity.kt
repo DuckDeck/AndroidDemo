23d2047b1054f94688707dcc8dbbf30f1e605a2b
“stanhu” <3421902@qq.com>
2019-04-17 14:41:56 +0800

    will finish dynamic image

--- app/src/main/java/stan/androiddemo/project/Mito/AboutMitoActivity.kt
+++ app/src/main/java/stan/androiddemo/project/Mito/AboutMitoActivity.kt
@@ -2,6 +2,7 @@ package stan.androiddemo.project.Mito
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.facebook.drawee.backends.pipeline.Fresco
 import kotlinx.android.synthetic.main.activity_about_mito.*
 
 
@@ -13,6 +14,11 @@ class AboutMitoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_mito)
         toolbar.setNavigationOnClickListener { onBackPressed() }
+
+        btn_clear_cache.setOnClickListener {
+            //清空缓存
+        }
+
 
     }
 
