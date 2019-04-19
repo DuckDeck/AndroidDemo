7b6120ad70bd9e4857ab0009a2f0c082bd7f4065
“stanhu” <3421902@qq.com>
2019-04-19 09:45:26 +0800

    process code

---
+++ app/src/main/java/stan/androiddemo/project/Mito/DynamicMitoActivity.kt
@@ -0,0 +1,13 @@
+package stan.androiddemo.project.Mito
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import stan.androiddemo.R
+
+class DynamicMitoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dynamic_mito)
+    }
+}
