69e21086f3c5ce4f5347b9e2f2cf41c3492fff2b
Stan Hu <3421902@qq.com>
2019-04-21 10:37:32 +0800

    123

--- app/src/main/java/stan/androiddemo/project/Mito/DynamicMitoPlayActivity.kt
+++ app/src/main/java/stan/androiddemo/project/Mito/DynamicMitoPlayActivity.kt
@@ -1,9 +1,12 @@
 package stan.androiddemo.project.Mito
 
+import android.app.DownloadManager
+import android.content.Context
 import android.net.Uri
 import android.opengl.Visibility
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.view.View
 import android.widget.MediaController
 import android.widget.Toast
@@ -11,6 +14,7 @@ import kotlinx.android.synthetic.main.activity_dynamic_mito_play.*
 import stan.androiddemo.Model.ResultInfo
 import stan.androiddemo.R
 import stan.androiddemo.project.Mito.Model.ImageSetInfo
+import java.io.File
 
 class DynamicMitoPlayActivity : AppCompatActivity() {
 
@@ -30,6 +34,22 @@ class DynamicMitoPlayActivity : AppCompatActivity() {
         }
 
         txt_download.setOnClickListener {
+
+            val request = DownloadManager.Request(Uri.parse(videoLink))
+            request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI)
+            request.setTitle("下载")
+            request.setDescription("正在下载动态壁纸$title")
+            request.setAllowedOverRoaming(true)
+
+
+            val pictureFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).absoluteFile
+            val fileDir = File(pictureFolder,"Mito")
+            if (!fileDir.exists()){
+                fileDir.mkdir()
+            }
+            request.setDestinationInExternalFilesDir(this,fileDir.absolutePath,"$title.mp4")
+            val downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+            val id = downloadManager.enqueue(request)
 
         }
 
