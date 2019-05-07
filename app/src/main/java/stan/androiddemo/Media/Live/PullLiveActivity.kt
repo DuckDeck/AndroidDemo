e9c27aeaf1830393aa9d624bcd851c95003ba01b
“stanhu” <3421902@qq.com>
2019-05-07 16:35:20 +0800

    123

--- app/src/main/java/stan/androiddemo/Media/Live/PullLiveActivity.kt
+++ app/src/main/java/stan/androiddemo/Media/Live/PullLiveActivity.kt
@@ -1,16 +1,29 @@
 package stan.androiddemo.Media.Live
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_pull_live.*
+import master.flame.danmaku.controller.DrawHandler
+import master.flame.danmaku.danmaku.model.BaseDanmaku
+import master.flame.danmaku.danmaku.model.DanmakuTimer
+import master.flame.danmaku.danmaku.model.IDanmakus
+import master.flame.danmaku.danmaku.model.IDisplayer
+import master.flame.danmaku.danmaku.model.android.DanmakuContext
+import master.flame.danmaku.danmaku.parser.BaseDanmakuParser
 import stan.androiddemo.R
 import tv.danmaku.ijk.media.player.IMediaPlayer
 import tv.danmaku.ijk.media.player.IjkMediaPlayer
 import java.lang.Exception
+import java.util.*
 
 class PullLiveActivity : AppCompatActivity() {
+
+
+
+    lateinit var  danmakuContext:DanmakuContext
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,6 +77,93 @@ class PullLiveActivity : AppCompatActivity() {
             }
         })
 
+
+
+
+
+        danmaku_view.setCallback(object:DrawHandler.Callback{
+            override fun prepared() {
+                danmaku_view.start()
+            }
+
+            override fun updateTimer(timer: DanmakuTimer?) {
+
+            }
+
+            override fun danmakuShown(danmaku: BaseDanmaku?) {
+
+            }
+
+            override fun drawingFinished() {
+            }
+        })
+
+
+
+
+        danmaku_view.enableDanmakuDrawingCache(true)
+
+        danmakuContext = DanmakuContext.create()
+        danmakuContext.setDanmakuStyle(IDisplayer.DANMAKU_STYLE_STROKEN,3.0f)
+        danmakuContext.isDuplicateMergingEnabled = false
+        danmakuContext.setScrollSpeedFactor(1.2f)
+        danmaku_view.prepare(getDeaultDanmakuParser(),danmakuContext)
+        danmaku_view.showFPS(true)
+
+
+    }
+
+    fun getDeaultDanmakuParser():BaseDanmakuParser{
+        return object:BaseDanmakuParser(){
+            override fun parse(): IDanmakus {
+                return danmakus
+            }
+        }
+    }
+
+
+    fun generateSomeDanmaku(){
+        for (i in 0 until 100){
+            val time = Random().nextInt(300)
+            val content = "" + time + time
+            addDanmaku(content,false)
+        }
+    }
+
+    fun addDanmaku(content:String,withBorder:Boolean){
+        val danmaku = danmakuContext.mDanmakuFactory.createDanmaku(BaseDanmaku.TYPE_SCROLL_RL)
+        danmaku.text = content
+        danmaku.padding = 5
+        danmaku.textSize = 20.0f
+        danmaku.textColor = Color.WHITE
+        val time = Random().nextInt(900)
+        danmaku.time = danmaku_view.currentTime + time * 10
+        if (withBorder){
+            danmaku.borderColor = Color.GREEN
+        }
+        danmaku_view.addDanmaku(danmaku)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (danmaku_view != null && danmaku_view.isPrepared){
+            danmaku_view.pause()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (danmaku_view != null && danmaku_view.isPrepared && danmaku_view.isPaused){
+           danmaku_view.resume()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (danmaku_view != null){
+            danmaku_view.release()
+
+        }
     }
 
     override fun onStop() {
