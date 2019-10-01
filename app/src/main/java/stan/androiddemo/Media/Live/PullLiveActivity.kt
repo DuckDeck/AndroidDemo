8d827900726137f03ab9950f10b42629f5d3c4b9
Stan Hu <3421902@qq.com>
2019-10-01 12:34:25 +0800

    fix windows issue

--- app/src/main/java/stan/androiddemo/Media/Live/PullLiveActivity.kt
+++ app/src/main/java/stan/androiddemo/Media/Live/PullLiveActivity.kt
@@ -6,14 +6,14 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_pull_live.*
-import master.flame.danmaku.controller.DrawHandler
-import master.flame.danmaku.danmaku.model.BaseDanmaku
-import master.flame.danmaku.danmaku.model.DanmakuTimer
-import master.flame.danmaku.danmaku.model.IDanmakus
-import master.flame.danmaku.danmaku.model.IDisplayer
-import master.flame.danmaku.danmaku.model.android.DanmakuContext
-import master.flame.danmaku.danmaku.model.android.Danmakus
-import master.flame.danmaku.danmaku.parser.BaseDanmakuParser
+//import master.flame.danmaku.controller.DrawHandler
+//import master.flame.danmaku.danmaku.model.BaseDanmaku
+//import master.flame.danmaku.danmaku.model.DanmakuTimer
+//import master.flame.danmaku.danmaku.model.IDanmakus
+//import master.flame.danmaku.danmaku.model.IDisplayer
+//import master.flame.danmaku.danmaku.model.android.DanmakuContext
+//import master.flame.danmaku.danmaku.model.android.Danmakus
+//import master.flame.danmaku.danmaku.parser.BaseDanmakuParser
 import stan.androiddemo.R
 import tv.danmaku.ijk.media.player.IMediaPlayer
 import tv.danmaku.ijk.media.player.IjkMediaPlayer
@@ -24,7 +24,7 @@ class PullLiveActivity : AppCompatActivity() {
 
 
 
-    lateinit var  danmakuContext:DanmakuContext
+   // lateinit var  danmakuContext:DanmakuContext
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -70,7 +70,7 @@ class PullLiveActivity : AppCompatActivity() {
 
             override fun onPrepared(p0: IMediaPlayer?) {
                 p0?.start()
-                generateSomeDanmaku()
+             //   generateSomeDanmaku()
             }
 
             override fun onSeekComplete(p0: IMediaPlayer?) {
@@ -83,100 +83,100 @@ class PullLiveActivity : AppCompatActivity() {
 
 
 
-
-        danmaku_view.setCallback(object:DrawHandler.Callback{
-            override fun prepared() {
-                danmaku_view.start()
-            }
-
-            override fun updateTimer(timer: DanmakuTimer?) {
-
-            }
-
-            override fun danmakuShown(danmaku: BaseDanmaku?) {
-
-            }
-
-            override fun drawingFinished() {
-            }
-        })
-
-
+//
+//        danmaku_view.setCallback(object:DrawHandler.Callback{
+//            override fun prepared() {
+//                danmaku_view.start()
+//            }
+//
+//            override fun updateTimer(timer: DanmakuTimer?) {
+//
+//            }
+//
+//            override fun danmakuShown(danmaku: BaseDanmaku?) {
+//
+//            }
+//
+//            override fun drawingFinished() {
+//            }
+//        })
 
 
-        danmaku_view.enableDanmakuDrawingCache(true)
 
-        danmakuContext = DanmakuContext.create()
-        danmakuContext.setDanmakuStyle(IDisplayer.DANMAKU_STYLE_STROKEN,3.0f)
-        danmakuContext.isDuplicateMergingEnabled = false
-        danmakuContext.setScrollSpeedFactor(1.2f)
-        danmaku_view.prepare(getDeaultDanmakuParser(),danmakuContext)
-        danmaku_view.showFPS(true)
+//
+//        danmaku_view.enableDanmakuDrawingCache(true)
+//
+//        danmakuContext = DanmakuContext.create()
+//        danmakuContext.setDanmakuStyle(IDisplayer.DANMAKU_STYLE_STROKEN,3.0f)
+//        danmakuContext.isDuplicateMergingEnabled = false
+//        danmakuContext.setScrollSpeedFactor(1.2f)
+//        danmaku_view.prepare(getDeaultDanmakuParser(),danmakuContext)
+//        danmaku_view.showFPS(true)
 
 
 
     }
 
-    fun getDeaultDanmakuParser():BaseDanmakuParser{
-        return object:BaseDanmakuParser(){
-            override fun parse(): IDanmakus {
-                return Danmakus()
-            }
-        }
-    }
+//    fun getDeaultDanmakuParser():BaseDanmakuParser{
+//        return object:BaseDanmakuParser(){
+//            override fun parse(): IDanmakus {
+//                return Danmakus()
+//            }
+//        }
+//    }
 
+//
+//    fun generateSomeDanmaku(){
+//        for (i in 0 until 100){
+//            val time = Random().nextInt(300)
+//            val content = "" + time + time
+//            addDanmaku(content,false)
+//        }
+//    }
 
-    fun generateSomeDanmaku(){
-        for (i in 0 until 100){
-            val time = Random().nextInt(300)
-            val content = "" + time + time
-            addDanmaku(content,false)
-        }
-    }
-
-    fun addDanmaku(content:String,withBorder:Boolean){
-        val danmaku = danmakuContext.mDanmakuFactory.createDanmaku(BaseDanmaku.TYPE_SCROLL_RL)
-        danmaku.text = content
-        danmaku.padding = 5
-        danmaku.textSize = 20.0f
-        danmaku.textColor = Color.WHITE
-        val time = Random().nextInt(900)
-        danmaku.time = danmaku_view.currentTime + time * 10
-        if (withBorder){
-            danmaku.borderColor = Color.GREEN
-        }
-        danmaku_view.addDanmaku(danmaku)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        if (danmaku_view != null && danmaku_view.isPrepared){
-            danmaku_view.pause()
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        if (danmaku_view != null && danmaku_view.isPrepared && danmaku_view.isPaused){
-           danmaku_view.resume()
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        if (danmaku_view != null){
-            danmaku_view.release()
-
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        IjkMediaPlayer.native_profileEnd()
-        if (danmaku_view != null){
-            danmaku_view.release()
-
-        }
-    }
+//    fun addDanmaku(content:String,withBorder:Boolean){
+//        val danmaku = danmakuContext.mDanmakuFactory.createDanmaku(BaseDanmaku.TYPE_SCROLL_RL)
+//        danmaku.text = content
+//        danmaku.padding = 5
+//        danmaku.textSize = 20.0f
+//        danmaku.textColor = Color.WHITE
+//        val time = Random().nextInt(900)
+//        danmaku.time = danmaku_view.currentTime + time * 10
+//        if (withBorder){
+//            danmaku.borderColor = Color.GREEN
+//        }
+//        danmaku_view.addDanmaku(danmaku)
+//    }
+//
+//    override fun onPause() {
+//        super.onPause()
+//        if (danmaku_view != null && danmaku_view.isPrepared){
+//            danmaku_view.pause()
+//        }
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//        if (danmaku_view != null && danmaku_view.isPrepared && danmaku_view.isPaused){
+//           danmaku_view.resume()
+//        }
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        if (danmaku_view != null){
+//            danmaku_view.release()
+//
+//        }
+//    }
+//
+//    override fun onStop() {
+//        super.onStop()
+//        IjkMediaPlayer.native_profileEnd()
+//        if (danmaku_view != null){
+//            danmaku_view.release()
+//
+//        }
+//    }
 
 }
